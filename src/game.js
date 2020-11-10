736bb7396a34a2c0606e40ccddba513062eb7131
Richard Tyler <richardtyler2011@gmail.com>
2020-11-09 17:42:19 -0700

    Refactor game.currentPlayer property

--- src/game.js
+++ src/game.js
@@ -2,7 +2,7 @@ class Game {
   constructor(player1, player2) {
     this.player1 = player1;
     this.player2 = player2;
-    this.currentPlayer = '';
+    this.currentPlayer;
     this.gameBoard = [null, null, null, null, null, null, null, null, null];
     this.winner;
     this.winningSequences = [
